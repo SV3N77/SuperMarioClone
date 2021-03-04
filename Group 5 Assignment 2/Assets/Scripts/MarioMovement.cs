47c4567239e19bf769f00cc0456df3c6d66956d7
EldridgeChan <65158216+EldridgeChan@users.noreply.github.com>
2021-03-04 19:03:23 +1100

    Implemented Mario Jump. Note that the jump need another script to check if onground

--- Group 5 Assignment 2/Assets/Scripts/MarioMovement.cs
+++ Group 5 Assignment 2/Assets/Scripts/MarioMovement.cs
@@ -4,40 +4,60 @@ using UnityEngine;
 
 public class MarioMovement : MonoBehaviour
 {
+    //These are the fields that can be controled by designer
     [Header("Movement Setting")]
     [Tooltip("The maximum X velocity of Mario")]
     [SerializeField]
     private float maxSpeed = 10.0f;
-    [Tooltip("The Addition value of Max Speed when running")]
+    [Tooltip("The Additional value of Max Speed when running")]
     [SerializeField]
     private float runMaxSpeed = 2.0f;
-    [Tooltip("The acceleration per Fixedframes of Mario")]
+    [Tooltip("The acceleration per second of Mario")]
     [SerializeField]
-    private float acceleration = 0.2f;
+    private float acceleration = 9.8f;
     [Tooltip("The Addition value of acceleration when running")]
     [SerializeField]
-    private float runAcceleration = 0.05f;
-    [Tooltip("The deceleration when Mario stop runing but still walking to the same direction")]
+    private float runAcceleration = 2.2f;
+    [Tooltip("The deceleration per second when Mario stop runing but still walking to the same direction")]
     [SerializeField]
-    private float autoDeceleration = 0.05f;
-    [Tooltip("The deceleration of Mario toward the inertial velocity When turning i.e. The acceleration toward the inputed direction which is opposite from the inertial velocity")]
+    private float autoDeceleration = 7.0f;
+    [Tooltip("The deceleration per second of Mario toward the inertial velocity When turning i.e. The acceleration toward the inputed direction which is opposite from the inertial velocity")]
     [SerializeField]
-    private float turningAcceleration = 0.3f;
-    [Tooltip("The deceleration of Mario when there is no left or right input")]
+    private float turningAcceleration = 15.0f;
+    [Tooltip("The deceleration per second of Mario when there is no left or right input")]
     [SerializeField]
-    private float deceleration = 0.15f;
+    private float deceleration = 8.0f;
 
     [Header("Jumping Setting")]
+    [Tooltip("The acceleration (units / second) at time = 0 in jump")]
     [SerializeField]
-    private float jumpVelocity;
+    private float jumpAcceleration = 10.0f;
+    [Tooltip("The increased value per second in Mario's gravity scale when falling (If no change to the gravity scale, change to 0.0f)")]
     [SerializeField]
-    private float jumpDeceleration;
+    private float jumpDeceleration = 1.0f;
+    [Tooltip("The maximun gravity scale when Mario is falling")]
+    [SerializeField]
+    private float jumpMaxDeceleration = 2.0f;
+    [Tooltip("The additional upward acceleration of Mario walk jump(Levelone jump) (Additional to stand jump(Levelzero jump))")]
+    [SerializeField]
+    private float levelOneJumpAcceleration = 1.0f;
+    [Tooltip("The additional upward acceleration of Mario run jump(Leveltwo jump) (Additional to stand jump(Levelzero jump))")]
+    [SerializeField]
+    private float levelTwoJumpAcceleration = 3.0f;
+    [Tooltip("The upper limit of Mario X velocity that he will perform a stand jump(Levelzero jump)")]
+    [SerializeField]
+    private float levelZeroJumpBoundary = 1.0f;
+    [Tooltip("The upper limit of Mario x velocity that he will perform a walk jump(Levelone jump). Also, while Mario x velocity > levelOneJumpBoundary, he will perform a run jump(Leveltwo jump)")]
+    [SerializeField]
+    private float levelOneJumpBoundary = 10.0f;
 
     [Header("Components")]
     [Tooltip("The Rigidbody2D Componenet of Mario")]
     [SerializeField]
     private Rigidbody2D marioRig;
 
+    //These fields are used for code and calculation
+    private bool onGround = false;
 
     private void Awake()
     {
@@ -52,19 +72,26 @@ public class MarioMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0.0f)
-        {
-            marioMovement(Input.GetAxisRaw("Horizontal"), Input.GetKey(KeyCode.L));
-        } else
-        {
-            marioDecelering();
-        }
+        marioFalling();
+
+        //Code for testing
+        //if (Input.GetAxisRaw("Horizontal") != 0.0f)
+        //{
+        //    marioMovement(Input.GetAxisRaw("Horizontal"), Input.GetKey(KeyCode.L));
+        //} else
+        //{
+        //    marioDecelering();
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //code for testing
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    marioJump();
+        //}
     }
 
     public void marioMovement(float dir, bool isRun)
@@ -77,16 +104,16 @@ public class MarioMovement : MonoBehaviour
             if (Mathf.Abs(marioRig.velocity.x) > maxSpeed && !isRun)
             {
                 //AutoDecelerate
-                marioRig.velocity = new Vector2(Mathf.Clamp(marioRig.velocity.x, -1.0f, 1.0f) * Mathf.Clamp(Mathf.Abs(marioRig.velocity.x) - autoDeceleration, maxSpeed, runMaxSpeed + maxSpeed), marioRig.velocity.y);
+                marioRig.velocity = new Vector2(Mathf.Clamp(marioRig.velocity.x, -1.0f, 1.0f) * Mathf.Clamp(Mathf.Abs(marioRig.velocity.x) - autoDeceleration * Time.fixedDeltaTime, maxSpeed, runMaxSpeed + maxSpeed), marioRig.velocity.y);
             } else
             {
                 //Normal Accelerate
-                marioRig.velocity = new Vector2(Mathf.Clamp(marioRig.velocity.x + dir * acc, -max, max), marioRig.velocity.y); 
+                marioRig.velocity = new Vector2(Mathf.Clamp(marioRig.velocity.x + dir * acc * Time.fixedDeltaTime, -max, max), marioRig.velocity.y); 
             }
         } else
         {
             //Turning Accelerate
-            marioRig.velocity = new Vector2(marioRig.velocity.x + dir * turningAcceleration, marioRig.velocity.y);
+            marioRig.velocity = new Vector2(marioRig.velocity.x + dir * turningAcceleration * Time.fixedDeltaTime, marioRig.velocity.y);
         }
 
     }
@@ -98,7 +125,57 @@ public class MarioMovement : MonoBehaviour
             marioRig.velocity = new Vector2(0.0f, marioRig.velocity.y);
         } else {
             float antiDir = -marioRig.velocity.x / Mathf.Abs(marioRig.velocity.x);
-            marioRig.velocity = new Vector2(marioRig.velocity.x + antiDir * deceleration, marioRig.velocity.y);
+            marioRig.velocity = new Vector2(marioRig.velocity.x + antiDir * deceleration * Time.fixedDeltaTime, marioRig.velocity.y);
         }
     }
+
+    public void marioJump()
+    {
+        if (onGround)
+        {
+            marioRig.velocity += Vector2.up * (jumpAcceleration + levelOfJump());
+            onGround = false;
+        }
+    }
+
+    public void marioFalling()
+    {
+        if (!onGround)
+        {
+            marioRig.gravityScale = Mathf.Clamp(marioRig.gravityScale + jumpDeceleration * Time.fixedDeltaTime, 1.0f, jumpMaxDeceleration);
+        }
+    }
+
+    public void resetMarioGravityScale()
+    {
+        marioRig.gravityScale = 1.0f;
+    }
+
+    private float levelOfJump()
+    {
+        float vel = Mathf.Abs(marioRig.velocity.x);
+        if (vel <= levelZeroJumpBoundary)
+        {
+            return 0.0f;
+        }
+        else if (vel < levelOneJumpBoundary)
+        {
+            return levelOneJumpAcceleration;
+        }
+        else
+        {
+            return levelTwoJumpAcceleration;
+        }
+    }
+
+    public void setOnGround(bool isGrounded)
+    {
+        onGround = isGrounded;
+    }
+
+    public float getMarioYVelocity()
+    {
+        return marioRig.velocity.y;
+    }
+
 }
